abbfaf6aaf669441ab64cb7599345954c1f30817
Tiago Danin <TiagoDanin@outlook.com>
2020-10-10 11:51:04 -0300

    feat: get products of store

--- database.js
+++ database.js
@@ -2,13 +2,32 @@ const jsonfile = require('jsonfile')
 const {v4: uuidv4} = require('uuid')
 
 const file = 'data.json'
-const bicos = jsonfile.readFileSync(file)
+let data = jsonfile.readFileSync(file)
 
 const save = () => {
-	jsonfile.writeFileSync(file, bicos, {
+	jsonfile.writeFileSync(file, data, {
 		replacer: true,
 		spaces: '\t'
 	})
 }
 
-module.exports = {}
+const getAllStore = () => data.allStores
+
+const getStore = (storeId) => {
+	return data.allStores.find(store => store.storeId === storeId)
+}
+
+const getAllProductsOfStore = (storeId) => {
+	return data.allProducts.filter(product => product.storeId === storeId)
+}
+
+const getProduct = (productId) => {
+	return data.allProducts.find(product => product.productId === productId)
+}
+
+module.exports = {
+	getAllStore,
+	getStore,
+	getAllProductsOfStore,
+	getProduct
+}
--- index.js
+++ index.js
@@ -15,5 +15,16 @@ app.get('/', (request, responseExpress) => responseExpress.send('Hello World!'))
 
 app.get('/api', (request, responseExpress) => responseExpress.json({isOk: true}))
 
+app.get('/products/:storeId', (request, responseExpress) => {
+	const { storeId } = request.params
+	responseExpress.json({
+		isOk: true,
+		bannerImgUrl: database.getStore(storeId).bannerImgUrl, 
+		allProducts: database.getAllProductsOfStore(storeId),
+		allStores: database.getAllStore()
+	})
+})
+
+app.get('/api', (request, responseExpress) => responseExpress.json({isOk: true}))
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
